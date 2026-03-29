9b82880f975f90d4a561a5b7f64a1953a5659bb6
admin <andreadelr.lc@gmail.com>
2026-03-29 17:09:59 -0500

    Creacion del Proyecto

---
+++ src/main/java/com/mycompany/cementerio/Cementerio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cementerio;
+
+/**
+ *
+ * @author admin
+ */
+public class Cementerio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
