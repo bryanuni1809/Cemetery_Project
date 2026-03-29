4e5f9d78f6685d9646f975ddb8855a1f7c694046
admin <andreadelr.lc@gmail.com>
2026-03-29 17:29:05 -0500

    Creacion de objeto Persoa

---
+++ src/main/java/Objetos/Persona.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objetos;
+
+/**
+ *
+ * @author admin
+ */
+public class Persona {
+    private String Nombre;
+    private String Apellido; 
+
+    public Persona(String Nombre, String Apellido) {
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    } 
+}
